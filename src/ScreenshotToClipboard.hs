44d993c70bf3bf116a6fb6c18e262af2ebe009b5
Dennis Gosnell <cdep.illabout@gmail.com>
2022-06-05 02:25:23 +0900

    Get working and add docs.

--- src/ScreenshotToClipboard.hs
+++ src/ScreenshotToClipboard.hs
@@ -3,9 +3,10 @@ module ScreenshotToClipboard where
 
 import Control.Monad (when)
 import Data.Functor (void)
+import Data.IORef (IORef, newIORef, atomicModifyIORef')
 import GI.Gdk (displayGetDefault)
 import GI.GdkPixbuf (pixbufNewFromFile)
-import GI.Gtk (clipboardGetDefault, clipboardStore, clipboardSetImage, onClipboardOwnerChange)
+import GI.Gtk (clipboardGetDefault, clipboardStore, clipboardSetCanStore, clipboardSetImage, onClipboardOwnerChange)
 import qualified GI.Gtk as Gtk
 import System.Exit (ExitCode(ExitSuccess))
 import System.FilePath ((</>))
@@ -17,35 +18,60 @@ defaultMain =
   withSystemTempDirectory "screenshot-to-clipboard" $ \tempdir -> do
     void $ Gtk.init Nothing
     let imgPath = tempdir </> "image.png"
+
+    -- Take screenshot with `import` program from imagemagick.
     (importExitCode, _, _) <- readProcessWithExitCode "import" [imgPath] ""
     when (importExitCode /= ExitSuccess) $
       err "error when calling `import` to take a screenshot"
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before get imgPixbuf? " <> show isEventsPending
+
+    -- Load screenshot into GDK Pixbuf.
     imgPixbuf <-
       maybe (err "could not load screenshot image file with gdk pixbuf") pure =<< pixbufNewFromFile imgPath
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before get display? " <> show isEventsPending
+
+    -- Get the current display, since it is needed to get the clipboard.
     display <-
       maybe (err "could not get default display from GDK") pure =<< displayGetDefault
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before get clipboard? " <> show isEventsPending
+
+    -- Get the clipboard and set hint that it should be able to store.
     clipboard <- clipboardGetDefault display
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before set on clipboard owner change callback? " <> show isEventsPending
-    void $ onClipboardOwnerChange clipboard $ \_ -> do
-      isEventsPending <- Gtk.eventsPending
-      putStrLn $ "onclipboard change callback, is events pending before mainQuit? " <> show isEventsPending
-      -- Gtk.mainQuit
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before get clipboard set image? " <> show isEventsPending
+    clipboardSetCanStore clipboard Nothing
+
+    -- Add callback for when the clipboard contents change.
+    numberOfCallsIORef <- newIORef 0
+    void $
+      onClipboardOwnerChange clipboard $
+        const (clipboardOwnerChangeCallback numberOfCallsIORef)
+
+    -- Load the screenshot into the clipboard and store it.
     clipboardSetImage clipboard imgPixbuf
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before get clipboard store? " <> show isEventsPending
     clipboardStore clipboard
-    isEventsPending <- Gtk.eventsPending
-    putStrLn $ "is events pending before run main? " <> show isEventsPending
+
+    -- Start GTK main loop.
     Gtk.main
   where
     err :: String -> a
     err e = error $ "screenshot-to-clipboard: " <> e
+
+    -- This callback is called everytime the clipboard changes.  It counts the
+    -- number of times it has been called, and it will end the program after
+    -- having been called two times.
+    --
+    -- The first time this function is called will be when this program copies
+    -- the screenshot to the clipboard.  This program then hangs around,
+    -- waiting for something else to be copied to the clipboard.  It tells the
+    -- GTK mainloop to exit only after having been called a second time.
+    --
+    -- The reason for this is because with XMonad (and other non-Gnome window
+    -- managers?), it appears that there is nothing that will persist the
+    -- screenshot after copying it to the clipboard if this program exits.  So
+    -- this program needs to continue running at leaset until ANOTHER program
+    -- actually pastes the screenshot that has been copied to the clipboard.
+    --
+    -- However, this program gets no notification when another program has
+    -- pasted the screenshot.  In order to work around this lack of
+    -- information, this program will just keep running until it sees that
+    -- something else has been copied to the clipboard.
+    clipboardOwnerChangeCallback :: IORef Int -> IO ()
+    clipboardOwnerChangeCallback numberOfCallsIORef = do
+      newNum <- atomicModifyIORef' numberOfCallsIORef (\num -> (num + 1, num + 1))
+      when (newNum >= 2) $ Gtk.mainQuit
